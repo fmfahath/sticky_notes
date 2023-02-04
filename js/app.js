a8d4ceec621b429d8accaa0e6b904285b0a60044
fmfahath <fmfahath.dev@gmail.com>
2023-02-04 17:32:54 +0530

    main/app.js : function for getting data from JSON local storage file

--- js/app.js
+++ js/app.js
@@ -1,2 +1,10 @@
+//gets elemnt's values
 const container = document.getElementById('container');
 const addBtn = document.getElementsByClassName('add-btn')[0];
+
+//get data from JSON file (from local storage)
+function getAppStorage(){
+    return JSON.parse(localStorage.getItem('joes-app') || "[]");
+}
+
+getAppStorage();
