78cb044de0c179b1ed2f9ef1b78135bc3b1e8871
fmfahath <fmfahath.dev@gmail.com>
2023-02-04 17:52:07 +0530

    main/app.js : fnction for creating new textarea element

--- js/app.js
+++ js/app.js
@@ -7,4 +7,10 @@ function getAppStorage(){
     return JSON.parse(localStorage.getItem('joes-app') || "[]");
 }
 
-getAppStorage();
+//create a new textarea element
+function createTextElement(id,contant){
+    const textElement = document.createElement('textarea');
+    textElement.classList.add('sticky');
+    textElement.value = contant;
+    return textElement;
+}
